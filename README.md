7310e7d22f89ddfbbeb8a56e1d8631b25e357463
Jakub Łabor <jacob.labor@gmail.com>
2023-03-24 18:50:49 +0100

    Implementation

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # key-migration
-Library for migrating Sui addresses to new version from user public keys
+
+Library for migrating Sui addresses to new version from user public keys.
+
+Projects which rely on persisting data across wipes generally rely on being able to restore data by airdropping objects into a list of saved addresses. This will break in `Sui 0.28` as the saved addresses will no longer be valid addresses. This library allows you to generate `pre-0.28` and `0.28` addresses for a user-provided public key so that they can be updated.
+
+This library exposes two functions:
+
+* `generateEd25519Address(public_key): [oldAddress, newAddress]`
+* `generateSecp256k1Address(public_key): [oldAddress, newAddress]`
+
+Correct working of the library can be verified using:
+
+```
+npm install
+node tests.js
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+import sha3 from 'js-sha3';
+import { blake2b } from '@noble/hashes/blake2b';
+import { bytesToHex } from '@noble/hashes/utils';
+import {
+    Ed25519PublicKey,
+    Secp256k1PublicKey,
+    SIGNATURE_SCHEME_TO_FLAG
+} from '@mysten/sui.js';
+
+function oldEd25519Address(publicKey) {
+    let tmp = new Uint8Array(Ed25519PublicKey.SIZE + 1);
+    tmp.set([SIGNATURE_SCHEME_TO_FLAG['ED25519']]);
+    tmp.set(publicKey.toBytes(), 1);
+    return sha3.sha3_256(tmp).slice(0, 40);
+}
+
+function newEd25519Address(publicKey) {
+    let tmp = new Uint8Array(Ed25519PublicKey.SIZE + 1);
+    tmp.set([SIGNATURE_SCHEME_TO_FLAG['ED25519']]);
+    tmp.set(publicKey.toBytes(), 1);
+    return bytesToHex(blake2b(tmp, { dkLen: 32 })).slice(0, 64);
+}
+
+export function generateEd25519Address(publicKey) {
+    if (!(publicKey instanceof Ed25519PublicKey)) {
+        throw new Error("publicKey was not instance of `Ed25519PublicKey`");
+    }
+
+    return [oldEd25519Address(publicKey), newEd25519Address(publicKey)];
+}
+
+function oldSecp256k1Address(publicKey) {
+    let tmp = new Uint8Array(Secp256k1PublicKey.SIZE + 1);
+    tmp.set([SIGNATURE_SCHEME_TO_FLAG['Secp256k1']]);
+    tmp.set(publicKey.toBytes(), 1);
+    return sha3.sha3_256(tmp).slice(0, 40);
+}
+
+function newSecp256k1Address(publicKey) {
+    let tmp = new Uint8Array(Secp256k1PublicKey.SIZE + 1);
+    tmp.set([SIGNATURE_SCHEME_TO_FLAG['Secp256k1']]);
+    tmp.set(publicKey.toBytes(), 1);
+    return bytesToHex(blake2b(tmp, { dkLen: 32 })).slice(0, 64);
+}
+
+export function generateSecp256k1Address(publicKey) {
+    if (!(publicKey instanceof Secp256k1PublicKey)) {
+        throw new Error("publicKey was not instance of `Secp256k1PublicKey`");
+    }
+
+    return [oldSecp256k1Address(publicKey), newSecp256k1Address(publicKey)];
+}
