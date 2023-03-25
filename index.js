1890ca495f43a249b7f6f2aa66b81c49d955849b
Jakub Łabor <jacob.labor@gmail.com>
2023-03-26 00:24:30 +0100

    Normalize addresses

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import sha3 from 'js-sha3';
 import { blake2b } from '@noble/hashes/blake2b';
 import { sha3_256 } from '@noble/hashes/sha3';
 import { bytesToHex } from '@noble/hashes/utils';
@@ -7,6 +6,11 @@ import {
     Secp256k1PublicKey,
     SIGNATURE_SCHEME_TO_FLAG
 } from '@mysten/sui.js';
+
+function normalize(value, addressLength) {
+    let address = value.toLowerCase();
+    return `0x${address.padStart(addressLength * 2, '0')}`;
+}
 
 function oldEd25519Address(publicKey) {
     let tmp = new Uint8Array(Ed25519PublicKey.SIZE + 1);
@@ -27,7 +31,10 @@ export function generateEd25519Address(publicKey) {
         throw new Error("publicKey was not instance of `Ed25519PublicKey`");
     }
 
-    return [oldEd25519Address(publicKey), newEd25519Address(publicKey)];
+    return [
+        normalize(oldEd25519Address(publicKey), 20),
+        normalize(newEd25519Address(publicKey), 32),
+    ];
 }
 
 function oldSecp256k1Address(publicKey) {
@@ -49,5 +56,8 @@ export function generateSecp256k1Address(publicKey) {
         throw new Error("publicKey was not instance of `Secp256k1PublicKey`");
     }
 
-    return [oldSecp256k1Address(publicKey), newSecp256k1Address(publicKey)];
+    return [
+        normalize(oldSecp256k1Address(publicKey), 20),
+        normalize(newSecp256k1Address(publicKey), 30),
+    ];
 }
--- tests.js
+++ tests.js
@@ -6,18 +6,18 @@ import { Ed25519PublicKey, Secp256k1PublicKey } from '@mysten/sui.js';
 const ED_TEST_CASES = [
     [
         'UdGRWooy48vGTs0HBokIis5NK+DUjiWc9ENUlcfCCBE=',
-        '3415400a4bfdf924aefa55446e5f4cd6e9a9399f',
-        'd77a6cd55073e98d4029b1b0b8bd8d88f45f343dad2732fc9a7965094e635c55'
+        '0x3415400a4bfdf924aefa55446e5f4cd6e9a9399f',
+        '0xd77a6cd55073e98d4029b1b0b8bd8d88f45f343dad2732fc9a7965094e635c55'
     ],
     [
         '0PTAfQmNiabgbak9U/stWZzKc5nsRqokda2qnV2DTfg=',
-        '2e6dad710b343b8655825bc420783aaa5ade08c2',
-        '7e8fd489c3d3cd9cc7cbcc577dc5d6de831e654edd9997d95c412d013e6eea23'
+        '0x2e6dad710b343b8655825bc420783aaa5ade08c2',
+        '0x7e8fd489c3d3cd9cc7cbcc577dc5d6de831e654edd9997d95c412d013e6eea23'
     ],
     [
         '6L/l0uhGt//9cf6nLQ0+24Uv2qanX/R6tn7lWUJX1Xk=',
-        '607a2403069d547c3fbba4b9e22793c7d78abb1f',
-        '3a1b4410ebe9c3386a429c349ba7929aafab739c277f97f32622b971972a14a2'
+        '0x607a2403069d547c3fbba4b9e22793c7d78abb1f',
+        '0x3a1b4410ebe9c3386a429c349ba7929aafab739c277f97f32622b971972a14a2'
     ],
 ];
 
@@ -37,23 +37,23 @@ function testEd25519() {
 let SECP_TEST_CASES = [
     [
         'AwTC3jVFRxXc3RJIFgoQcv486QdqwYa8vBp4bgSq0gsI',
-        '35057079b5dfc60d650768e2f4f92318f4ea5a77',
-        'cdce00b4326fb908fdac83c35bcfbda323bfcc0618b47c66ccafbdced850efaa'
+        '0x35057079b5dfc60d650768e2f4f92318f4ea5a77',
+        '0xcdce00b4326fb908fdac83c35bcfbda323bfcc0618b47c66ccafbdced850efaa'
     ],
     [
         'A1F2CtldIGolO92Pm9yuxWXs5E07aX+6ZEHAnSuKOhii',
-        '0187cf4234ff80862d5a1665d840df400fef29a0',
-        'b588e58ed8967b6a6f9dbce76386283d374cf7389fb164189551257e32b023b2'
+        '0x0187cf4234ff80862d5a1665d840df400fef29a0',
+        '0xb588e58ed8967b6a6f9dbce76386283d374cf7389fb164189551257e32b023b2'
     ],
     [
         'Ak5rsa5Od4T6YFN/V3VIhZ/azMMYPkUilKQwc+RiaId+',
-        '70eaff6b7973c57842c2272f00aa19af9f20dc1b',
-        '694dd74af1e82b968822a82fb5e315f6d20e8697d5d03c0b15e0178c1a1fcfa0'
+        '0x70eaff6b7973c57842c2272f00aa19af9f20dc1b',
+        '0x694dd74af1e82b968822a82fb5e315f6d20e8697d5d03c0b15e0178c1a1fcfa0'
     ],
     [
         'A4XbJ3fLvV/8ONsnLHAW1nORKsoCYsHaXv9FK1beMtvY',
-        'deb28f733d9f59910cb210d56a46614f9dd28360',
-        '78acc6ca0003457737d755ade25a6f3a144e5e44ed6f8e6af4982c5cc75e55e7'
+        '0xdeb28f733d9f59910cb210d56a46614f9dd28360',
+        '0x78acc6ca0003457737d755ade25a6f3a144e5e44ed6f8e6af4982c5cc75e55e7'
     ],
 ];
 
